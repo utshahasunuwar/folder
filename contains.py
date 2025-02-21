d8c3485614bd89994f5c29bd4d677a7a49594af9
Utshaha Sunuwar <utsahalohukuch@gmail.com>
2025-02-21 08:11:02 +0545

    Second Commit

---
+++ contains.py
@@ -0,0 +1,7 @@
+a = 1
+b = [1, 2, 3]
+if a in b:
+    print('b contains a')
+else:
+    print('b does not contain a')
+
---
+++ equals.py
@@ -0,0 +1,6 @@
+x = 5
+y = 7
+if x is y:
+    print('They are equal.')
+else:
+    print('They are not equal.')
